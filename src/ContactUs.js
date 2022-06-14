822801125c545d9281ab02576d100f8fa6688907
Mohanpirasath <mahendran.mohanmass2000@gmail.com>
2022-06-15 05:24:37 +0530

    contactus

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -7,13 +7,49 @@ import Select from '@mui/material/Select';
 import Fab from '@mui/material/Fab';
 import TextField from '@mui/material/TextField';
 import { Navigate, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 export function ContactUs() {
+
+
+
+  const formvalidationSchema = yup.object({
+    name: yup
+      .string()
+      .required("Name is required ⚠️")
+      .min(3, "Name must be at least 3️⃣ characters long"),
+    Address: yup
+      .string()
+      .required("Address is required ⚠️")
+      .min(15, "Address must be at least 15 characters long"),
+    // price: yup
+    //   .string()
+    //   .required(" is required ⚠️")
+    //   .max(9999999999, "price must be at least  characters long"),
+    Contactnum: yup
+      .string()
+      .required("Contact number is required ⚠️")
+      .min(3, "image must be at least 3️ characters long"),
+    EmailID: yup.string().required("plz fill the email ").min(5,"email must be lengther"),
+    bar: yup.string().required("plz select any one").min(15),
+  });
+
+  const { handleSubmit, values, handleChange, handleBlur, errors, touched } = useFormik({
+    initialValues: { name: "", Address: "", Contactnum: "", EmailID: "", bar: "" },
+    validationSchema: formvalidationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+
+
 
   const [CATEGORY, setAge] = React.useState('');
   const navigate=useNavigate()
 
-  const handleChange = (event) => {
+  const handleChange1 = (event) => {
     setAge(event.target.value);
   };
 
@@ -42,22 +78,46 @@ export function ContactUs() {
           <TextField
             required
             id="outlined-required"
+            name="name"
+            onChange={handleChange}
+            error={errors.name && touched.name}
+            helperText={errors.name && touched.name ? errors.name : ""}
+            onBlur={handleBlur}
+            value={values.name}
             label="Name"
             placeholder="Name" />
           <TextField
             required
             id="outlined-required"
+            name="Address"
+            onChange={handleChange}
+            error={errors.Address && touched.Address}
+            helperText={errors.Address && touched.Address ? errors.Address : ""}
+            onBlur={handleBlur}
+            value={values.Address}
             label="Address"
             placeholder="Address" />
           <TextField
             required
             id="outlined-required"
+            name="Contactnum"
+            onChange={handleChange}
+            error={errors.Contactnum && touched.Contactnum}
+            helperText={errors.Contactnum && touched.Contactnum ? errors.Contactnum : ""}
+            onBlur={handleBlur}
+            value={values.Contactnum}
             type="number"
             label="Contact Number"
             placeholder="Number" />
           <TextField
             required
             id="outlined-required"
+            name="EmailID"
+            onChange={handleChange}
+            error={errors.EmailID && touched.EmailID}
+            helperText={errors.EmailID && touched.EmailID ? errors.EmailID : ""}
+            onBlur={handleBlur}
+            value={values.EmailID}
             type="email"
             label="E-Mail"
             placeholder="E-mail id" />
@@ -70,7 +130,7 @@ export function ContactUs() {
                 id="demo-simple-select"
                 value={CATEGORY}
                 label="CATEGORY"
-                onChange={handleChange}
+                onChange={handleChange1}
               >
                 <MenuItem value={"cam"}>Camera</MenuItem>
                 <MenuItem value={"car"}>Car's</MenuItem>
@@ -84,6 +144,12 @@ export function ContactUs() {
           <TextField
             // required
             id="outlined-required"
+            name="bar"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={errors.bar && touched.bar}
+            helperText={errors.bar && touched.bar ? errors.bar : ""}
+            value={values.bar}
             label="convertion bar"
             placeholder="" />
 
